69e623524481c83babbb70d30873d7386d646650
sledgej <jonathan.sledge@schencksc.com>
2017-02-28 09:42:30 -0600

    ui tweaks and some styling fixes

--- src/app/tiering/components/client-tier-details/client-tier-details.component.ts
+++ src/app/tiering/components/client-tier-details/client-tier-details.component.ts
@@ -44,7 +44,7 @@ export class ClientTierDetailsComponent implements OnInit {
     this.scoreRanges.ServiceTouch = this.route.snapshot.data['serviceTouch'];
     this.scoreRanges.Payment = this.route.snapshot.data['paymentScore'];
     this.scoreRanges.Tier = this.route.snapshot.data['tierScore'];
-    console.log(this.scoreRanges.WorkTiming);
+
     this.displayData.Billings = this.parentAnalysisData.Billings;
     this.displayData.PaymentTimeliness = this.parentAnalysisData.PaymentTimeliness;
     this.displayData.PeakPercent = this.parentAnalysisData.PeakPercent;
