070cc0f534c72c16661e92fc6554c4a1542657d4
sledgej <jonathan.sledge@schencksc.com>
2017-03-09 10:39:57 -0600

    removed dash

--- src/app/teamMember/teamMember.service.spec.ts
+++ src/app/teamMember/teamMember.service.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { TeamMemberService } from './team-member.service';
+import { TeamMemberService } from './teamMember.service';
 
 describe('TeamMemberService', () => {
   beforeEach(() => {
