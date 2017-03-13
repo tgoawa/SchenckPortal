a7d711336c8b1df28fc5828a6fb3e93d4f3b7bd0
sledgej <jonathan.sledge@schencksc.com>
2017-03-13 11:02:45 -0500

    new form on add plan instead of on init

--- src/app/businessDevelopment/components/strategyPlan/strategyPlan.component.ts
+++ src/app/businessDevelopment/components/strategyPlan/strategyPlan.component.ts
@@ -8,7 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class StrategyPlanComponent implements OnInit {
 
-  sideMenuItemId: number = 2; //Tell side menu the active menu item
+  sideMenuItemId = 2; //Tell side menu the active menu item
 
   private addPlanMode = false;
   private strategyPlanForm: FormGroup;
@@ -17,15 +17,15 @@ export class StrategyPlanComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.strategyPlanForm = this.fb.group({
-      StrategyPlanId: [0],
-      strategyPlanTitle: ['', [Validators.required, Validators.maxLength(75)]],
-      knownAs: [''],
-      famous: ['']
-    });
   }
 
   addPlanButton() {
     this.addPlanMode = true;
+     this.strategyPlanForm = this.fb.group({
+      StrategyPlanId: [0],
+      strategyPlanTitle: ['', [Validators.required, Validators.maxLength(75)]],
+      knownAs: [''],
+      famous: ['', Validators.maxLength(200)]
+    });
   }
 }
