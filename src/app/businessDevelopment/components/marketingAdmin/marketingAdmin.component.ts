ccf9d2b0077c611d20d83db5b241ec58c5491f25
sledgej <jonathan.sledge@schencksc.com>
2017-03-23 09:34:06 -0500

    constructing basic layout of page

--- src/app/businessDevelopment/components/marketingAdmin/marketingAdmin.component.ts
+++ src/app/businessDevelopment/components/marketingAdmin/marketingAdmin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { TeamMemberService } from '../../../teamMember/teamMember.service';
 
 @Component({
   selector: 'app-marketing-admin',
@@ -8,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class MarketingAdminComponent implements OnInit {
 
   sideMenuItemId = 2; //Tell side menu the active menu index
+
+  private teamMemberId: number;
+  private marketingMemberList: FormGroup;
   
-  constructor() { }
+  constructor(private fb: FormBuilder, private teamMember: TeamMemberService) { }
 
   ngOnInit() {
+    this.teamMemberId = this.teamMember.teamMember.TeamMemberId;
+    this.setMarketingMemberForm();
   }
 
+  setMarketingMemberForm() {
+    this.marketingMemberList = this.fb.group({
+      marketingMember: this.teamMemberId
+    });
+  }
 }
