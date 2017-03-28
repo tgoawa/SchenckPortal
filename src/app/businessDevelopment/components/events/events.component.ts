d849597aaf47dd73c0324847c4beda6965434467
sledgej <jonathan.sledge@schencksc.com>
2017-03-28 08:36:49 -0500

    added edit event item functionality

--- src/app/businessDevelopment/components/events/events.component.ts
+++ src/app/businessDevelopment/components/events/events.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { ModalDirective } from 'ng2-bootstrap/modal';
-import { DropDownDataService,
-          StrategyEventService } from '../../services/';
+import {
+  DropDownDataService,
+  StrategyEventService
+} from '../../services/';
 import { IStrategyEvent, DropDownData } from '../../models/';
 
 @Component({
@@ -12,7 +14,7 @@ import { IStrategyEvent, DropDownData } from '../../models/';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-  @ViewChild('createEventItem') public createEventItem: ModalDirective;
+  @ViewChild('EventItemModal') public EventItemModal: ModalDirective;
   @Input() private currentPlanId: number;
   @Input() private currentEvents: IStrategyEvent[];
 
@@ -30,37 +32,28 @@ export class EventsComponent implements OnInit {
     this.newEventItemForm();
   }
 
-  showEventModal() {
-    this.createEventItem.show();
-    this.modalTitle = 'Create';
+  getStatusData() {
+    this.dds.getEventStatuses()
+      .then((data: DropDownData[]) => this.eventStatus = data)
+      .catch(this.handleError);
   }
 
-  hideEventModal() {
-    this.createEventItem.hide();
-  }
-
-  addEventItem({value, valid}: { value: IStrategyEvent, valid: boolean} ) {
-    let eventId = this.eventForm.get('EventId').value;
-    if (eventId === 0) {
-      this.eventService.createEvent(value)
-      .then(data => {
-        this.currentEvents.push(data);
-        this.existingEvents = true;
-      })
-      .catch(this.handleError);
-      this.hideEventModal();
-      this.eventForm.reset();
-    } else {
-      this.eventService.updateEvent(value)
-      .then(data => this.currentEvents = data)
-      .catch(this.handleError);
-      this.hideEventModal();
-      this.eventForm.reset();
+  isExistingEvents() {
+    if (this.currentEvents[0].EventId !== 0) {
+      this.existingEvents = true;
     }
   }
 
-  eventSetComplete() {
-    this.isCompleted = true;
+  newEvent() {
+    this.modalTitle = 'Create';
+    this.newEventItemForm();
+    this.EventItemModal.show();
+  }
+
+  editEvent(event) {
+    this.modalTitle = 'Update';
+    this.editEventForm(event);
+    this.EventItemModal.show();
   }
 
   newEventItemForm() {
@@ -75,17 +68,46 @@ export class EventsComponent implements OnInit {
     });
   }
 
-  getStatusData() {
-    this.dds.getEventStatuses()
-    .then((data: DropDownData[]) => this.eventStatus = data)
-    .catch(this.handleError);
+  editEventForm(event: IStrategyEvent) {
+    this.eventForm = this.fb.group({
+      PlanId: event.PlanId,
+      EventId: event.EventId,
+      Name: [event.Name, Validators.required],
+      ScheduledDate: [event.ScheduledDate, Validators.required],
+      Description: [event.Description, [Validators.required, Validators.maxLength(200)]],
+      StatusId: [event.StatusId, Validators.required],
+      Feedback: [event.Feedback]
+    });
   }
 
-  isExistingEvents() {
-    if (this.currentEvents[0].EventId !== 0) {
-      this.existingEvents = true;
+  addEventItem({ value, valid }: { value: IStrategyEvent, valid: boolean }) {
+    let eventId = this.eventForm.get('EventId').value;
+    if (eventId === 0) {
+      this.eventService.createEvent(value)
+        .then(data => {
+          this.currentEvents.push(data);
+          this.existingEvents = true;
+        })
+        .catch(this.handleError);
+      this.hideEventModal();
+      this.eventForm.reset();
+    } else {
+      this.eventService.updateEvent(value)
+        .then(data => this.currentEvents = data)
+        .catch(this.handleError);
+      this.hideEventModal();
+      this.eventForm.reset();
     }
   }
+
+  eventSetComplete() {
+    this.isCompleted = true;
+  }
+
+  hideEventModal() {
+    this.EventItemModal.hide();
+  }
+
   private handleError(error: any) {
     let errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
