f75dbf9c93838ff9581b68379543933274f64ffe
sledgej <jonathan.sledge@schencksc.com>
2017-03-29 08:46:28 -0500

    added community section to plan

--- src/app/businessDevelopment/components/outreachAndInterests/outreachAndInterests.component.ts
+++ src/app/businessDevelopment/components/outreachAndInterests/outreachAndInterests.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ModalDirective } from 'ng2-bootstrap/modal';
 
 @Component({
   selector: 'app-outreach-interests',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./outreachAndInterests.component.css']
 })
 export class OutreachInterestsComponent implements OnInit {
-
+  @ViewChild('communityModal') public communityModal: ModalDirective; 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showCommunityModal() {
+    this.communityModal.show();
+  }
+
+  hideCommunityModal() {
+    this.communityModal.hide();
   }
 
 }
