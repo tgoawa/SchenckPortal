5da1987e0d75b70f0149e54cbda5ca7321412fdd
sledgej <jonathan.sledge@schencksc.com>
2017-04-10 13:28:52 -0500

    Mentor admin displays all mentorships and mentor name.
    Fix #2, Fix #3

--- src/app/businessDevelopment/marketingAdmin/models/mentor.dto.ts
+++ src/app/businessDevelopment/marketingAdmin/models/mentor.dto.ts
@@ -1,11 +1,17 @@
+import { TeamMember } from '../../../teamMember';
+
 export interface IMentor {
     MentorshipId: number;
     MentorId: number;
     TeamMemberId: number;
+    Mentor: TeamMember;
+    TeamMember: TeamMember;
 }
 
 export class MentorDTO implements IMentor {
     MentorshipId: number;
     MentorId: number;
     TeamMemberId: number;
+    Mentor: TeamMember;
+    TeamMember: TeamMember;
 }
